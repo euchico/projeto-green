875595c4bce90330184c6a9579145da0461b86eb
Francisco de Paula <franciscodepaula.web@gmail.com>
2023-09-03 00:15:18 -0300

    Reorganizaçao

---
+++ projeto-backend/Lottery/Lottery.cs
@@ -0,0 +1,19 @@
+﻿using Lottery.View;
+
+namespace Lottery;
+
+class Lottery
+{
+    //public const string USER_FILE_PATH = @"C:\Users\eufra\Desenvolvimento\3-github\100diasdecodigo\GameBook\Data\UserRecord.txt";
+    //public const string BOOK_FILE_PATH = @"C:\Users\eufra\Desenvolvimento\3-github\100diasdecodigo\GameBook\Data\BookRecord.txt";
+
+    static void Main(string[] args)
+    {
+        //Screen mainScreen = new ScreenStart("início");
+        //mainScreen.ScreenLoop();
+
+        Screen screen = new();
+        screen.ShowLogo();
+        screen.ShowScreenTitle("Teste Teste");
+    }
+}
---
+++ projeto-backend/Lottery/View/Screen.cs
@@ -0,0 +1,49 @@
+﻿namespace Lottery.View;
+
+class Screen
+{
+    private string ScreenTitle { get; }
+    private bool StatusLoop { get; set; }
+
+    internal Screen(string screenTitle)
+    {
+        ScreenTitle = screenTitle!;
+        StatusLoop = true;
+
+        SetConsole();
+        ShowHeader(ScreenTitle);
+    }
+
+    void SetConsole()
+    {
+        Console.Title = "Lottery";
+    }
+
+    void ShowLogo()
+    {
+        Console.WriteLine(@"
+█░░ █▀█ ▀█▀ ▀█▀ █▀▀ █▀█ █▄█
+█▄▄ █▄█ ░█░ ░█░ ██▄ █▀▄ ░█░");
+        Console.WriteLine();
+    }
+
+    void ShowScreenTitle(string screenTitle)
+    {
+        string topBorder = string.Empty.PadRight(screenTitle.Length + 4, '▄');
+        string bottomBorder = string.Empty.PadRight(screenTitle.Length + 4, '▀');
+
+        Console.WriteLine(topBorder);
+        Console.WriteLine($"▐ {screenTitle.ToUpper()} ▌");
+        Console.WriteLine(bottomBorder + "\n");
+    }
+
+    void ShowHeader(string screenTitle)
+    {
+        Console.Clear();
+
+        ShowLogo();
+        ShowScreenTitle(screenTitle);
+    }
+
+    internal abstract bool ScreenLoop();
+}
